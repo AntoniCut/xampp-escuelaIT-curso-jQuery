66c388196c2128855779a120f2e0458a677e7b33
Antonio <antonicut@gmail.com>
2025-04-20 11:58:05 +0200

    Refactorización - Cambiar estructura de carpetas y nombres de archivos del curso

---
+++ src/libs/jquery/cdn/cdn-jquery-1.11.0.js
@@ -0,0 +1,13 @@
+//  --------------------------------------------
+//  ----------  cdn-jquery-1.11.0.js  ----------
+//  --------------------------------------------
+
+
+//  ----------  Configuración de jQuery por CDN  ----------
+export const cdnJQuery_1_11_0 = {
+
+    srcCdn: "https://code.jquery.com/jquery-1.11.0.min.js",
+    integrity: "sha256-spTpc4lvj4dOkKjrGokIrHkJgNA0xMS98Pw9N7ir9oI=",
+    crossOrigin: "anonymous",
+    referrerPolicy: "no-referrer",
+}
---
+++ src/libs/jquery/cdn/cdn-jquery-3.6.3.js
@@ -0,0 +1,13 @@
+//  -------------------------------------------
+//  ----------  cdn-jquery-3.6.3.js  ----------
+//  -------------------------------------------
+
+
+//  ----------  Configuración de jQuery por CDN  ----------
+export const cdnJQuery_3_6_3 = {
+
+    srcCdn: "https://code.jquery.com/jquery-3.6.3.min.js",
+    integrity: "sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=",
+    crossOrigin: "anonymous",
+    referrePolicy: "no-referrer",
+}
